ad1b737e389c6229f97e3978f76d059fe3c01b68
IKshv <ikshvartsman@gmail.com>
2024-02-24 06:44:01 -0800

    f

--- stream_camera.py
+++ stream_camera.py
@@ -1,12 +1,10 @@
-from flask import Flask, Response, request
+from flask import Flask, Response
 from flask_httpauth import HTTPBasicAuth
 import cv2
 import platform
 import logging
 from logging.handlers import RotatingFileHandler
 import os
-import threading
-import datetime
 
 # Initialize Flask application
 app = Flask(__name__)
@@ -29,55 +27,6 @@ def verify_password(username, password):
         return users.get(username) == password
     return False
 
-def save_video():
-    os_name = platform.system()
-    camera_index = 0
-    if os_name == "Windows":
-        cap = cv2.VideoCapture(camera_index)
-    elif os_name == "Darwin":
-        cap = cv2.VideoCapture(camera_index, cv2.CAP_AVFOUNDATION)
-    elif os_name == "Linux":
-        cap = cv2.VideoCapture(camera_index)
-
-    # Check if the camera is opened successfully
-    if not cap.isOpened():
-        logger.error("Failed to open camera for video recording")
-        return
-
-    # Define video codec and create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'avc1')  # H.264 codec for MOV format
-    out = cv2.VideoWriter(f'output_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.mov', fourcc, 20.0, (640, 480))
-
-    # Memory buffer for smoother recording
-    frame_buffer = []
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret:
-            # Compress frame and add to buffer
-            ret, buffer = cv2.imencode('.jpg', frame)
-            if ret:
-                frame_buffer.append(buffer)
-
-            # Write frames from buffer to video file
-            if len(frame_buffer) >= 10:  # Write every 10 frames
-                for buf in frame_buffer:
-                    frame = cv2.imdecode(buf, cv2.IMREAD_COLOR)
-                    out.write(frame)
-                frame_buffer = []
-
-        else:
-            break
-
-    # Write remaining frames from buffer
-    for buf in frame_buffer:
-        frame = cv2.imdecode(buf, cv2.IMREAD_COLOR)
-        out.write(frame)
-
-    # Release resources
-    cap.release()
-    out.release()
-
 def generate_frames():
     os_name = platform.system()
     camera_index = 0
@@ -94,45 +43,24 @@ def generate_frames():
             logger.error("Failed to capture video frame")
             break
         else:
-            # Invert the frame
-            frame = cv2.flip(frame, 1)  # Flip both vertically and horizontally
-
-            # Encode the inverted frame
             ret, buffer = cv2.imencode('.jpg', frame)
-            if not ret:
-                logger.error("Failed to encode video frame")
-                break
-
-            # Convert the frame to bytes and yield for streaming
+            frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
-
-    # Release resources
-    cap.release()
-
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video')
 @auth.login_required
 def video():
-    client_ip = request.remote_addr
-    user_agent = request.headers.get('User-Agent')
-    logger.info(f"Video stream requested by {client_ip} using {user_agent}")
+    logger.info("Video stream requested")
     try:
         return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
     except Exception as e:
-        logger.error(f"Error serving video stream to {client_ip}: {e}")
+        logger.error(f"Error serving video stream: {e}")
         raise
 
 if __name__ == '__main__':
-    video_thread = threading.Thread(target=save_video)
-    video_thread.start()
     app.run(host='0.0.0.0', port=5001, debug=True)
 
-
-
-'''
-
-# Path: stream_camera.py'''
 
 # from flask import Flask, Response, request
 # from flask_httpauth import HTTPBasicAuth
